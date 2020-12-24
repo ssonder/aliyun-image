e575c119800b2c055df7ffa02b0eb39a2b525c4f
ssonder <402250695@qq.com>
2020-12-24 12:02:30 +0800

    update

--- scalabel/scripts/install_go_packages.sh
+++ scalabel/scripts/install_go_packages.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-go get \
+go get -u \
     github.com/aws/aws-sdk-go github.com/mitchellh/mapstructure \
     gopkg.in/yaml.v2 \
     github.com/satori/go.uuid github.com/dgrijalva/jwt-go \
-    github.com/360EntSecGroup-Skylar/excelize/v2
+    github.com/360EntSecGroup-Skylar/excelize/v2 \
+    github.com/360EntSecGroup-Skylar/excelize
 
 curl -sfL https://install.goreleaser.com/github.com/golangci/golangci-lint.sh \
     | sh -s -- -b $(go env GOPATH)/bin v1.17.1
